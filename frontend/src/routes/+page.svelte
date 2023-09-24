ae23112047a8d37ff78b2d78d7d48a9d94fc50e1
tokitou-san <tokitousan2005@gmail.com>
2023-09-24 06:09:48 +0530

    style: basic chat ui layout

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Telegram Redition</h1>
+<main>
+	<sidebar>
+		sidebar
+	</sidebar>
+	<chat>
+		chat
+	</chat>
+</main>
+
+<style lang="postcss">
+	main {
+		display: grid;
+		grid-template-columns: 400px auto;
+	}
+</style>
