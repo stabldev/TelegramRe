4eeb0df51c9f6009bd4efc7e5c24f4cfd5068ace
tokitou-san <tokitousan2005@gmail.com>
2023-09-24 08:04:04 +0530

    style: add SCSS styling lang

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 		<div class="sidebar-head">
 			<div class="menu">
 				<button>
-					<Menu />
+					<Menu style="width: 2rem;" />
 				</button>
 			</div>
 			<div class="search">
@@ -20,18 +20,38 @@
 	</chat>
 </main>
 
-<style lang="postcss">
+<style lang="scss">
 	main {
 		display: grid;
-		grid-template-columns: 400px auto;
+		grid-template-columns: 30rem auto;
 	}
 
-	/* Sidebar */
 	.sidebar {
 		width: 100%;
-	}
+		height: 100vh;
+		background: #212121;
 
-	.sidebar-head {
-		display: flex;
+		.sidebar-head {
+			display: flex;
+			align-items: center;
+			padding: 0.75rem 1rem;
+
+			.menu button {
+				cursor: pointer;
+				background: none;
+				border: none;
+				outline: none;
+				color: #aaaaaa;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				border-radius: 50%;
+				padding: 0.5rem;
+
+				&:hover {
+					background: #2B2B2B;
+				}
+			}
+		}
 	}
 </style>
