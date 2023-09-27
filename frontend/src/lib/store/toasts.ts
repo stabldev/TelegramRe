ab1bcb43d2e1357693f6289e1c8d1e3b9469b331
tokitou-san <tokitousan2005@gmail.com>
2023-09-27 12:45:01 +0530

    refactor: type annotations and format codes

--- frontend/src/lib/store/toasts.ts
+++ frontend/src/lib/store/toasts.ts
@@ -1,30 +1,30 @@
-import { writable } from "svelte/store";
-import type { Writable } from "svelte/store";
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 
 type Toast = {
 	id?: number;
-	message: string;
+	message?: string;
 	timeout?: number;
 };
 
 export const toasts: Writable<Toast[]> = writable([]);
 
-export const addToast = (toast: Toast) => {
+export const addToast = (toast: Toast): void => {
 	// generate random id to make "dismissToast" function more efficient
 	const id = Math.floor(Math.random() * 10000);
-	// toast defaults
-	const defaults = {
+	// toast defaults props
+	const toast_defaults: Toast = {
 		id,
-		timeout: 3000 // default timeout
+		timeout: 3000 // default timeout "3s"
 	};
 
-	const merged_toast = {...defaults, ...toast};
+	const merged_toast: Toast = { ...toast_defaults, ...toast };
 
 	toasts.update((toasts) => [...toasts, merged_toast]);
 	// dismiss toast after timeout
-	setTimeout(() => dismissToast(merged_toast.id), merged_toast.timeout);
+	setTimeout(() => dismissToast(id), merged_toast.timeout);
 };
 
-export const dismissToast = (id: number) => {
+export const dismissToast = (id: number): void => {
 	toasts.update((toasts) => toasts.filter((toast) => toast.id !== id));
 };
