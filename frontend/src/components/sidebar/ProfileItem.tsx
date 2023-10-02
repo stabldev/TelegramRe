3aa197d4bdb07f5bc1eb559c90dd771aee3ccb43
tokitou-san <tokitousan2005@gmail.com>
2023-10-02 11:41:34 +0530

    Refactor: add solid/router and logic for active profile

--- frontend/src/components/sidebar/ProfileItem.tsx
+++ frontend/src/components/sidebar/ProfileItem.tsx
@@ -1,4 +1,7 @@
-type ProfileItem = {
+import { A } from "@solidjs/router";
+import { FormatDate } from "../../functions/format_date";
+
+export type ProfileItemType = {
 	image: string;
 	name: string;
 	username: string;
@@ -7,19 +10,25 @@ type ProfileItem = {
 	new_message: boolean;
 };
 
-export default function ProfileItem(props: ProfileItem) {
+export function ProfileItem(props: ProfileItemType) {
+	const formated_timestamp = new FormatDate(props.timestamp).format_to_relative_time;
+
 	return (
-		<a href={`/@${props.username}`} class="flex items-center gap-[1vw] px-[1vw] py-[0.75vw] bg-stone-700">
+		<A
+			href={`/@${props.username}`}
+			class="w-full flex items-center gap-[1vw] px-[1vw] py-[0.75vw] hover:bg-stone-800"
+			activeClass="bg-stone-700 hover:!bg-stone-700"
+		>
 			<img class="w-[3.5vw] rounded-full" src={props.image} alt={props.name} />
-			<div class="flex flex-col">
+			<div class="flex flex-col w-full">
 				<div class="flex items-center justify-between">
 					<span class="text-[1.1vw] font-medium text-white">{props.name}</span>
-					<span class="uppercase text-white/75 text-[0.8vw]">{props.timestamp}</span>
+					<span class="uppercase text-white/75 text-[0.8vw]">{formated_timestamp}</span>
 				</div>
 				<div>
 					<span class="line-clamp-1 text-white/75 text-[1vw]">{props.message}</span>
 				</div>
 			</div>
-		</a>
+		</A>
 	);
 };
--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -1,20 +1,35 @@
+import { For } from "solid-js";
 import { FormatDate } from "../../functions/format_date";
-import ProfileItem from "./ProfileItem";
+import { ProfileItem, ProfileItemType } from "./ProfileItem";
 import SearchHeader from "./SearchHeader";
+
+const profile_mapping: ProfileItemType[] = [
+	{
+		name: "Anya Forger",
+		username: "anya-forger",
+		image: "https://pm1.aminoapps.com/8063/ff1db42bbc3a7bc249022b37125da8fa3b1e2d4br1-512-512v2_hq.jpg",
+		message: "Hi wassup! I've something to tell you, so please reply when you're free",
+		timestamp: "2023-10-02T05:21:26Z",
+		new_message: false
+	},
+	{
+		name: "Toshinou Kyouko",
+		username: "kyouko",
+		image: "https://pbs.twimg.com/media/D2v3DBuXQAAMFIb.jpg",
+		message: "Here is my gift for your birthday <3",
+		timestamp: "2023-10-01T05:21:26Z",
+		new_message: false
+	}
+];
 
 export default function Sidebar() {
 	return (
 		<div class="bg-stone-900 w-full">
 			<SearchHeader />
 			<div>
-				<ProfileItem
-					image="https://pm1.aminoapps.com/8063/ff1db42bbc3a7bc249022b37125da8fa3b1e2d4br1-512-512v2_hq.jpg"
-					name="Anya Forger"
-					username="anya-forger"
-					message="Hi wassup! I've something to tell you, so please reply when you're free"
-					timestamp={String(new FormatDate("2023-10-02T05:21:26Z").format_to_relative_time)}
-					new_message={false}
-				/>	
+				<For each={profile_mapping}>
+					{(profile) => <ProfileItem {...profile} />}
+				</For>
 			</div>
 		</div>
 	);
