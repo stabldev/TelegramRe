07d8a94793bd5775f647c0c4b038a9a6d134ed65
tokitou-san <tokitousan2005@gmail.com>
2023-10-02 16:33:23 +0530

    Feat: add type for icons

--- frontend/src/assets/icons/Tick.tsx
+++ frontend/src/assets/icons/Tick.tsx
@@ -1,8 +1,9 @@
 import { Show } from "solid-js";
+import { VariantIcon } from "../../types/Icon";
 
-export default function Tick(props: { variant: "single" | "double"; class?: string; }) {
+export default function Tick(props: VariantIcon) {
 	return (
-		<svg class={props.class} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="1em" width="1em">
+		<svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="1em" width="1em">
 			<Show
 				when={props.variant === "single"}
 				fallback={<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.1" d="m1.75 9.75l2.5 2.5m3.5-4l2.5-2.5m-4.5 4l2.5 2.5l6-6.5"/>}
