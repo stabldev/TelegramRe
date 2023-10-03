ce452235f9ecb575a8d9907e92e16ff66e46ce84
tokitou-san <tokitousan2005@gmail.com>
2023-10-04 00:21:43 +0530

    Refactor: add named import in AppRouter

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "@solidjs/router";
-import { lazy } from "solid-js";
+import { Component, lazy } from "solid-js";
 
 // lazy imports
 const Chat = lazy(() => import("../screens/chat"));
 
-export default function AppRouter() {
+export const AppRouter: Component = () => {
 	return (
 		<Routes>
 			<Route path="/:username" component={Chat} />
