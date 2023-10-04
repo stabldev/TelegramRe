07de0300add0ec49452937999b421a169996f45e
tokitou-san <tokitousan2005@gmail.com>
2023-10-04 09:30:37 +0530

    Feat: add submit message form behaviour

--- frontend/src/screens/chat/ChatInput.tsx
+++ frontend/src/screens/chat/ChatInput.tsx
@@ -7,21 +7,41 @@ import Send from "../../assets/icons/Send";
 export const ChatInput: Component = () => {
 	const [message, setMessage]: Signal<string> = createSignal("");
 
+	const handleSubmit = (event: SubmitEvent) => {
+		event.preventDefault();
+		/* logic for adding message */
+
+		/* logic for adding message */
+		setMessage("");
+	};
+
 	return (
-		<div class="h-[3.75vw] flex gap-[1vw] items-center bg-stone-900 px-[1vw]">
-			<button class="text-[1.75vw] text-white/50 hover:text-white/75 transition-colors">
+		<form onSubmit={handleSubmit} class="h-[3.75vw] flex gap-[1vw] items-center bg-stone-900 px-[1vw]">
+			<button
+				type="button"
+				class="text-[1.75vw] text-white/50 hover:text-white/75 transition-colors"
+			>
 				<Clip />
 			</button>
 			<input
+				value={message()}
 				onInput={(e) => setMessage(e.currentTarget.value)}
 				type="type"
+				autocomplete="off"
+				name="message"
 				placeholder="Write a message..."
 				class="flex-1 border-none outline-none bg-transparent text-white text-[1vw]"
 			/>
-			<button class="text-[1.5vw] text-white/50 hover:text-white/75 transition-colors">
+			<button
+				type="button"
+				class="text-[1.5vw] text-white/50 hover:text-white/75 transition-colors"
+			>
 				<Emoji />
 			</button>
-			<button class="text-[1.65vw] text-white/50 hover:text-white/75 transition-colors">
+			<button
+				type="submit"
+				class="text-[1.65vw] text-white/50 hover:text-white/75 transition-colors"
+			>
 				<Show
 					when={message()}
 					fallback={<Mic />}
@@ -29,6 +49,6 @@ export const ChatInput: Component = () => {
 					<Send />
 				</Show>
 			</button>
-		</div>
+		</form>
 	);
 };
