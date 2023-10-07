ffaf85fc5905e36b624cb778358d0cf5c926bcba
tokitou-san <tokitousan2005@gmail.com>
2023-10-07 13:31:11 +0530

    Fix: chat block whitespace | padding | size

--- frontend/src/components/chat/ChatBlock.tsx
+++ frontend/src/components/chat/ChatBlock.tsx
@@ -18,31 +18,31 @@ export const ChatBlock = (props: Props) => {
 
 	return (
 		<div
-			class="text-white pl-[0.9vw] pr-[0.5vw] h-[2.15vw] flex gap-[0.5vw] w-max"
+			class="text-white pl-[0.9vw] pr-[0.5vw] py-[0.4vw] flex gap-[0.5vw] w-max"
 			classList={{
 				"bg-stone-800": !self,
 				"bg-blue-500": self
 			}}
 			style={{ "border-radius": `${lastMessage || middleMessage ? "0.35vw" : "1vw"} 1vw 1vw 0.35vw` }}
 		>
-			<span class="self-center text-[0.95vw]">{message.content}</span>
-			<span class="self-end pb-[0.15vw] text-[0.75vw] uppercase text-white/80 select-none">
+			<span class="self-center text-[0.95vw] whitespace-pre-line">{message.content}</span>
+			<span class="self-end leading-none text-[0.75vw] uppercase text-white/80 select-none">
 				{formatedDate}
 			</span>
 			<Show when={self}>
 				<Switch fallback={
-					<CLock class="self-end pb-[0.2vw] text-white text-[1.2vw]" />
+					<CLock class="self-end text-white text-[1vw]" />
 				}>
 					<Match when={message.status === "seen"}>
 						<Tick
 							variant="double"
-							class="self-end pb-[0.2vw] text-white text-[1.35vw]"
+							class="self-end text-white text-[1.15vw]"
 						/>
 					</Match>
 					<Match when={message.status === "send"}>
 						<Tick
 							variant="single"
-							class="self-end pb-[0.2vw] text-white text-[1.2vw]"
+							class="self-end text-white text-[1vw]"
 						/>
 					</Match>
 				</Switch>
