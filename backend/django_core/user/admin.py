8aa7533bee6c2b9a05f434a22f2f3d69f527a1cd
tokitou-san <tokitousan2005@gmail.com>
2023-10-12 17:38:04 +0530

    Feat: add customUser admin fields

--- backend/django_core/user/admin.py
+++ backend/django_core/user/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+USER_MODEL: type[CustomUser] = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+	model = USER_MODEL
+
+	list_display = (
+		"get_username",
+		"first_name",
+		"last_name",
+		"is_staff"
+	)
+
+	list_filter = (
+		"is_active",
+		"is_staff"
+	)
+
+	readonly_fields =  (
+		"date_joined",
+		"last_login"
+	)
