b41cb537f30a42936baa43445d40330c50e428f5
tokitou-san <tokitousan2005@gmail.com>
2023-10-15 11:55:25 +0530

    Refactor: add custom user admin form

---
+++ backend/django_core/user/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+USER_MODEL: type[CustomUser] = get_user_model()
+
+class CustomUserChangeForm(UserChangeForm):
+	class Meta:
+		model = USER_MODEL
+		fields = "__all__"
+
+class CustomUserCreationForm(UserCreationForm):
+	class Meta:
+		model = USER_MODEL
+		fields = "__all__"
