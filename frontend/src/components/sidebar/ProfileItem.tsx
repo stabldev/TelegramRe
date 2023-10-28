ffcd1c49aefcac0bf19e98ffdd333e3ce2ac863e
tokitouq <tokitousan2005@gmail.com>
2023-10-28 12:14:28 +0530

    add active style

--- frontend/src/components/sidebar/ProfileItem.tsx
+++ frontend/src/components/sidebar/ProfileItem.tsx
@@ -10,8 +10,8 @@ export const ProfileItem = (props: ProfileItemProps) => {
 	return (
 		<A
 			href={`/@${username()}`}
-			class="flex w-full select-none items-center gap-[1vw] px-[1vw] py-[0.75vw] hover:bg-stone-800"
-			activeClass="bg-stone-700 hover:!bg-stone-700"
+			class="flex w-full select-none items-center gap-[1vw] px-[1vw] py-[0.75vw] hover:bg-stone-800 rounded-[0.75vw]"
+			activeClass="bg-stone-700 hover:!bg-stone-700 before:content-[''] before:w-[0.35vw] before:h-[3vw] before:bg-blue-500 before:rounded-full before:absolute before:left-[1vw]"
 		>
 			<img
 				class="w-[3.5vw] rounded-full"
