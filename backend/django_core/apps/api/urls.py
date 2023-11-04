a77af452ce9db363e977f7fd6de7b8a3ca1f9ab4
tokitouq <tokitousan2005@gmail.com>
2023-11-04 08:34:25 +0530

    fix urls

--- backend/django_core/apps/api/urls.py
+++ backend/django_core/apps/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.api.views import ProfileDetailView
 
 urlpatterns = [
-	path("user/<id:int>/", ProfileDetailView.as_view(), name="profile_view"),
+	path("user/<int:pk>/", ProfileDetailView.as_view(), name="profile_view"),
 ]
--- backend/django_core/core/urls.py
+++ backend/django_core/core/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("api/", include("api.urls")),
+    path("api/", include("apps.api.urls")),
     path("admin/", admin.site.urls),
 ]
 
