3e825d6dfd5fe17b7c319867035958cc1d4f1c67
tokitouq <tokitousan2005@gmail.com>
2023-11-04 06:06:23 +0530

    add chat model

--- backend/django_core/apps/chat/models.py
+++ backend/django_core/apps/chat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from django_core.apps.user.models import CustomUser
+
 # Create your models here.
+class ChatMessage(models.Model):
+	user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, related_name="user")
+	sender = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, related_name="sender")
+	reciever = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, related_name="reciever")
+
+	message = models.TextField()
+	is_read = models.BooleanField(default=False)
+	date = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		# DB
+		db_table = "chat_message"
+		db_table_comment = "Chat messages"
+
+		ordering = ["date"]
+		get_latest_by = ["order_date"]
+
+	def __str__(self) -> str:
+		return f"{self.sender} - {self.reciever}"
+
+	@property
+	def get_sender(self):
+		sender = next(iter(CustomUser.objects.filter(id=self.user.id)), None)
+		return sender
+
+	@property
+	def get_reciever(self):
+		reciever = next(iter(CustomUser.objects.filter(id=self.user.id)), None)
+		return reciever
