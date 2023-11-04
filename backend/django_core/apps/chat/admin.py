f1fae28abd280965869cfe6329e0dfa00c087b67
tokitouq <tokitousan2005@gmail.com>
2023-11-04 06:09:57 +0530

    [skip deploy]: format code

--- backend/django_core/apps/chat/admin.py
+++ backend/django_core/apps/chat/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from .models import ChatMessage
 
+
 # Register your models here.
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
-	list_display = ["user", "sender", "reciever", "message", "is_read"]
-	list_editable = ["message", "is_read"]
+    list_display = ["user", "sender", "reciever", "message", "is_read"]
+    list_editable = ["message", "is_read"]
