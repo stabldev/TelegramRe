258591aba7dea988824d9a31338e5bff01bb213e
tokitouq <tokitousan2005@gmail.com>
2023-11-04 08:36:53 +0530

    fix api fields

--- backend/django_core/apps/api/serializers.py
+++ backend/django_core/apps/api/serializers.py
@@ -6,4 +6,4 @@ from apps.user.models import CustomUser
 class ProfileSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CustomUser
-		fields = "__all__"
+		fields = ["id", "username", "email", "first_name", "last_name", "is_verified", "avatar", "bio", "last_login", "date_joined"]
