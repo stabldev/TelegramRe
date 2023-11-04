80da84d43bcd7071d03900e88c9d484fd73fe772
tokitouq <tokitousan2005@gmail.com>
2023-11-04 08:40:56 +0530

    add permission classes

--- backend/django_core/apps/api/views.py
+++ backend/django_core/apps/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from apps.api.serializers import ProfileSerializer
 from apps.user.models import CustomUser
@@ -9,3 +10,4 @@ from apps.user.models import CustomUser
 class ProfileDetailView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
     queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
