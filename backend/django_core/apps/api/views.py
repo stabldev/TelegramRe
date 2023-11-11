0148d5ae9e3f851a003e1191738009a35fb76b79
tokitouq <tokitousan2005@gmail.com>
2023-11-11 23:25:25 +0530

    feat: simple inbox view

--- backend/django_core/apps/api/views.py
+++ backend/django_core/apps/api/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from apps.api.serializers import ProfileSerializer
+from apps.api.serializers import ProfileSerializer, ChatMessageSerializer
 from apps.user.models import CustomUser
+from apps.chat.models import ChatMessage
 
 
 # Create your views here.
@@ -11,3 +12,7 @@ class ProfileDetailView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
+
+class InboxView(generics.ListAPIView):
+    serializer_class = ChatMessageSerializer
+    queryset = ChatMessage.objects.all()
