c81eac552166d03ba44b3825195e932231859e58
tokitouq <tokitousan2005@gmail.com>
2023-11-11 23:24:58 +0530

    set inbox url path

--- backend/django_core/apps/api/urls.py
+++ backend/django_core/apps/api/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from apps.api import views
 
 urlpatterns = [
+    # Chat
+    path("inbox/<int:pk>/", views.InboxView.as_view(), name="inbox"),
+    # Profile
     path("user/<int:pk>/", views.ProfileDetailView.as_view(), name="profile_view"),
 ]
