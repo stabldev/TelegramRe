fef81ea301286e25e89f8177de761521cc319b14
tokitouq <tokitousan2005@gmail.com>
2023-11-11 08:57:04 +0530

    fix: `get_message` on admin

--- backend/django_core/apps/chat/admin.py
+++ backend/django_core/apps/chat/admin.py
@@ -6,5 +6,8 @@ from .models import ChatMessage
 # Register your models here.
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ["user", "sender", "reciever", "message", "is_read"]
-    list_editable = ["message", "is_read"]
+    list_display = ("user", "sender", "reciever", "get_message", "is_read")
+
+    def get_message(self, obj):
+        return f"{obj.message[0:50]}..."
+    get_message.short_description = "message"
