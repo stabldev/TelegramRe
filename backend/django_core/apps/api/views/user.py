34e5d42269892c1fceadec0496c2e84c0e92f6d9
tokitouq <tokitousan2005@gmail.com>
2023-11-19 15:47:13 +0530

    fix: renamed imports

--- backend/django_core/apps/api/views/user.py
+++ backend/django_core/apps/api/views/user.py
@@ -4,11 +4,11 @@ from django.db.models import OuterRef, Subquery, Q
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from apps.api.serializers import ProfileSerializer
+from apps.api.serializers import CustomUserSerializer
 from apps.user.models import CustomUser
 
 
 class ProfileDetailView(generics.RetrieveAPIView):
-    serializer_class = ProfileSerializer
+    serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
