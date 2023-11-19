31d41dc7069b509a8fa3a4d1adecfea5827323ba
tokitouq <tokitousan2005@gmail.com>
2023-11-19 12:19:51 +0530

    fix serializer

--- backend/django_core/apps/api/serializers.py
+++ backend/django_core/apps/api/serializers.py
@@ -20,25 +20,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "date_joined",
         ]
 
-
-class ChatMessageSerializer(serializers.ModelSerializer):
-    sender_id = serializers.IntegerField(source="sender.id", read_only=True)
-    sender_username = serializers.CharField(source="sender.username", read_only=True)
-    sender_full_name = serializers.SerializerMethodField()
-    sender_avatar = serializers.ImageField(source="sender.avatar", read_only=True)
-
+class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatMessage
-        fields = [
-            "id",
-            "message",
-            "is_read",
-            "date",
-            "sender_id",
-            "sender_username",
-            "sender_full_name",
-            "sender_avatar",
-        ]
-
-    def get_sender_full_name(self, obj):
-        return obj.sender.get_full_name()
+        fields = ['id','sender', 'reciever', 'message', 'is_read', 'date']
--- backend/django_core/apps/api/views/chat.py
+++ backend/django_core/apps/api/views/chat.py
@@ -1,44 +1,39 @@
-from django.db.models import OuterRef, Subquery, Q
+from django.db.models import Q, OuterRef, Subquery
 
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from apps.api.serializers import ChatMessageSerializer
+from apps.api.serializers import MessageSerializer
 from apps.user.models import CustomUser
 from apps.chat.models import ChatMessage
 
 
 class InboxView(generics.ListAPIView):
-    serializer_class = ChatMessageSerializer
+    serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user_id = self.kwargs.get("pk")
         messages = ChatMessage.objects.filter(
-            id__in=Subquery(
+            id__in =  Subquery(
                 CustomUser.objects.filter(
-                    Q(sender__reciever=user_id) | Q(reciever__sender=user_id)
-                )
-                .distinct()
-                .annotate(
+                    Q(sender__reciever=user_id) |
+                    Q(reciever__sender=user_id)
+                ).distinct().annotate(
                     last_msg=Subquery(
                         ChatMessage.objects.filter(
-                            Q(sender=OuterRef("id"), reciever=user_id)
-                            | Q(reciever=OuterRef("id"), sender=user_id)
-                        )
-                        .order_by("-id")[:1]
-                        .values_list("id", flat=True)
+                            Q(sender=OuterRef('id'),reciever=user_id) |
+                            Q(reciever=OuterRef('id'),sender=user_id)
+                        ).order_by('-id')[:1].values_list('id',flat=True) 
                     )
-                )
-                .values_list("last_msg", flat=True)
-                .order_by("-id")
+                ).values_list('last_msg', flat=True).order_by("-id")
             )
         ).order_by("-id")
         return messages
 
 
 class MessagesView(generics.ListAPIView):
-    serializer_class = ChatMessageSerializer
+    serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
