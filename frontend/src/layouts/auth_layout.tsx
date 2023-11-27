392d77a699b8613e55bbf53b9f234e6b7fb5e860
tokitouq <tokitousan2005@gmail.com>
2023-11-27 19:48:11 +0530

    feat: add login page layout and fix auth layout

--- frontend/src/layouts/auth_layout.tsx
+++ frontend/src/layouts/auth_layout.tsx
@@ -1,10 +1,14 @@
 import { JSX } from "solid-js";
+import { cn } from "~/functions/cn";
 
-export function AuthLayout(props: { children?: JSX.Element }) {
+export function AuthLayout(props: { children?: JSX.Element; class?: string }) {
 	return (
-		<main class="relative w-screen h-screen grid grid-cols-[1fr_1fr]">
-			<div></div>
-			<div class="grid place-items-center bg-stone-100">
+		<main class="relative w-screen h-screen grid grid-cols-[1fr_1fr] text-stone-900 leading-none">
+			<div
+				class="bg-no-repeat bg-cover bg-center"
+				style={{"background-image": "url(/images/auth-bg-01.jpg)"}}
+			></div>
+			<div class={cn(props.class, "flex flex-col items-start justify-center bg-stone-100 md:px-[12vw]")}>
 				{ props.children }
 			</div>
 		</main>
