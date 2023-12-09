b8b8bb2045bf485b8188323e4184f166d2a6fd5d
tokitouq <tokitousan2005@gmail.com>
2023-12-09 22:33:10 +0530

    fix: add types in tailwind config file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+} satisfies Config;
