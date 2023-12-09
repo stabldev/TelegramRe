cdd05d32c5c925a227a990054962501d32882809
tokitouq <tokitousan2005@gmail.com>
2023-12-09 23:54:33 +0530

    fix: metadata

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -11,11 +11,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Telegram Web",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "A Telegram inspired web messaging experience",
     },
   ],
 };
