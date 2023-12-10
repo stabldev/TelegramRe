2f003f27f75912945e820e4040ba7e2cc3e354c2
tokitouq <tokitousan2005@gmail.com>
2023-12-10 08:57:55 +0530

    fix: dynamic head

--- frontend/src/routes/[username]/index.tsx
+++ frontend/src/routes/[username]/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead, useLocation } from "@builder.io/qwik-city";
 import Chat from "~/components/pages/chat";
 import DefaultLayout from "~/layouts/default-layout";
 
@@ -9,3 +10,15 @@ export default component$(() => {
     </DefaultLayout>
   )
 })
+
+export const head: DocumentHead = ({ params }) => {
+  return {
+    title: params.username,
+    meta: [
+      {
+        name: "description",
+        content: "Chatting with @hello"
+      }
+    ]
+  }
+}
