c3f84cee484036a20089f91985c0508e6f4747b0
tokitouq <tokitousan2005@gmail.com>
2023-12-10 16:23:35 +0530

    fix: chat input component

--- frontend/src/components/pages/chat/index.tsx
+++ frontend/src/components/pages/chat/index.tsx
@@ -1,10 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import ChatHeader from "./chat-header";
+import ChatInput from "./chat-input";
 
 export default component$(() => {
   return (
     <div class="relative grid grid-rows-[min-content_1fr]">
       <ChatHeader />
+      <ChatInput />
     </div>
   )
 })
