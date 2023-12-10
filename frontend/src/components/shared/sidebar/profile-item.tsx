9e5c03aae517567a0b3b973985dbc2ee2b3da280
tokitouq <tokitousan2005@gmail.com>
2023-12-10 14:18:34 +0530

    fix: active link

--- frontend/src/components/shared/sidebar/profile-item.tsx
+++ frontend/src/components/shared/sidebar/profile-item.tsx
@@ -1,25 +1,33 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Link, useLocation } from "@builder.io/qwik-city";
 import { FormatDate } from "~/functions/format_date";
 import type { SidebarProfile } from "~/types/components/sidebar-profile";
 
 export default component$<SidebarProfile>((props) => {
+  const location = useLocation();
+
   const formated_timestamp = new FormatDate(props.timestamp).format_to_relative_time;
+  const href = `/@${props.username}/`;
 
   return (
     <>
       <Link
-        href={`/@${props.username}`}
-        class="flex w-full select-none items-center gap-[1vw] rounded-[0.75vw] px-[1vw] py-[0.75vw]"
+        href={href}
+        class={[
+          "group flex w-full select-none items-center gap-[1vw] rounded-[0.75vw] px-[1vw] py-[0.75vw] transition",
+          {
+            "bg-stone-700 hover:!bg-stone-700 before:absolute before:left-[1vw] before:w-[0.35vw] before:h-[3vw] before:bg-blue-500 before:rounded-full": location.url.pathname === href,
+          }
+        ]}
       >
         <img
-          class="w-[3.5vw] rounded-full"
+          class="w-[3.5vw] md:rounded-full"
           src={props.avatar}
           alt={props.name}
         />
         <div class="flex w-full flex-col">
           <div class="flex items-center justify-between">
-            <span class="text-[1.1vw] font-medium text-white">{props.name}</span>
+            <span class="text-[1vw] font-medium text-white">{props.name}</span>
             <span class="text-[0.8vw] uppercase text-white/75">{formated_timestamp}</span>
           </div>
           <div>
