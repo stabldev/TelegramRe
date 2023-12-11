22dd176e4a5c999463cef5bfc8d77a7197e9ee91
tokitouq <tokitousan2005@gmail.com>
2023-12-11 13:08:48 +0530

    refactor: chat area size

--- frontend/src/components/pages/chat/chat-area.tsx
+++ frontend/src/components/pages/chat/chat-area.tsx
@@ -13,7 +13,7 @@ export default component$<Props>((props) => {
     <div class="relative flex items-end">
       <div
         ref={props.ref}
-        class="flex w-full flex-col gap-[0.5vw] overflow-y-scroll pb-[4.5vw] px-[1vw] pt-[5vw] [scrollbar-width:_thin] [scrollbar-color:_rgba(255,255,255,0.1)_transparent]"
+        class="md:px-[15vw] flex w-full flex-col gap-[0.5vw] overflow-y-scroll pb-[4.5vw] px-[1vw] pt-[5vw] [scrollbar-width:_thin] [scrollbar-color:_rgba(255,255,255,0.1)_transparent]"
         style={{ "max-height": "calc(100vh - 3.75vw)" }}
       >
         {
