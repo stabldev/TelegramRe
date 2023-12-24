1adf0bdc35dcb81dd31e0c1de52280414648ec90
tokitouq <tokitousan2005@gmail.com>
2023-12-24 09:33:06 +0530

    format

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,33 +3,33 @@ module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
-		    animation: {
-		      	enter: "enter .2s ease-out",
-		      	leave: "leave .15s ease-in forwards",
-		    },
-		    keyframes: {
-		      	enter: {
-		        	"0%": {
-		          		opacity: "0",
-		          		transform: "scale(.9)",
-		        	},
-		        	"100%": {
-		          		opacity: "1",
-		          		transform: "scale(1)",
-		        	},
-		      	},
-		      	leave: {
-		        	"0%": {
-		          		opacity: "1",
-		          		transform: "scale(1)",
-		        	},
-		        	"100%": {
-		          		opacity: "0",
-		          		transform: "scale(.9)",
-		        	},
-		      	},
-		    },
-		},
+			animation: {
+				enter: "enter .2s ease-out",
+				leave: "leave .15s ease-in forwards"
+			},
+			keyframes: {
+				enter: {
+					"0%": {
+						opacity: "0",
+						transform: "scale(.9)"
+					},
+					"100%": {
+						opacity: "1",
+						transform: "scale(1)"
+					}
+				},
+				leave: {
+					"0%": {
+						opacity: "1",
+						transform: "scale(1)"
+					},
+					"100%": {
+						opacity: "0",
+						transform: "scale(.9)"
+					}
+				}
+			}
+		}
 	},
 	plugins: []
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,5 @@ import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		solid(),
-	]
+	plugins: [solid()]
 });
