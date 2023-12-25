920e629a65cdee02b7622f8c860d0ac5fa6b03f8
tokitouq <tokitousan2005@gmail.com>
2023-12-25 08:16:04 +0530

    fix: active selector

--- frontend/src/components/shared/sidebar/profile-item.tsx
+++ frontend/src/components/shared/sidebar/profile-item.tsx
@@ -20,7 +20,7 @@ export const ProfileItem = (props: ProfileItemProps) => {
 		<A
 			href={`/@${username()}`}
 			class="flex w-full select-none items-center gap-[1vw] rounded-[0.75vw] px-[1vw] py-[0.75vw]"
-			activeClass="bg-blue-500 hover:bg-blue-500 before:absolute before:left-[1vw] before:w-[0.35vw] before:h-[3vw] before:bg-blue-500 before:rounded-full"
+			activeClass="bg-blue-500 hover:bg-blue-500 before:absolute before:left-[1vw] before:w-[0.35vw] before:h-[2.5vw] before:bg-white before:rounded-full before:rounded-l-none"
 		>
 			<img
 				class="w-[3.5vw] rounded-full"
