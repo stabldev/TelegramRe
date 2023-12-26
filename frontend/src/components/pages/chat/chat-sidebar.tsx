d30041aadf6efcc1c1be5c4e672d08b6331348bd
tokitouq <tokitousan2005@gmail.com>
2023-12-26 09:40:34 +0530

    fix

--- frontend/src/components/pages/chat/chat-sidebar.tsx
+++ frontend/src/components/pages/chat/chat-sidebar.tsx
@@ -7,7 +7,10 @@ export const ChatSidebar = () => {
     const params = useParams<{ username: string }>();
 
     return <>
-        <div class="h-full w-full bg-stone-900 border-l-[0.1vw] border-black/50 whitespace-nowrap">
+        <div
+            class="h-full md:w-full bg-stone-900 border-l-[0.1vw] border-black/50 whitespace-nowrap"
+            style={{ "max-height": "calc(100vh - 3.75vw)" }}
+        >
             <div class="md:p-[1vw] flex items-center justify-between">
                 <h3 class="md:text-[1.25vw] text-stone-200 font-medium flex items-center md:gap-[0.75vw]">
                     Profile
@@ -24,6 +27,10 @@ export const ChatSidebar = () => {
             <div class="flex flex-col md:p-[1vw] leading-none md:gap-[0.5vw]">
                 <h3 class="text-stone-100 md:text-[1.25vw] font-medium">Anya Forger</h3>
                 <h5 class="text-stone-400 md:text-[1vw]">Joined Date: Dec 25, 2023</h5>
+            </div>
+            <div class="md:hidden flex flex-col md:p-[1vw] leading-none md:gap-[0.5vw]">
+                <h5 class="text-stone-400 md:text-[1vw]">BIO:</h5>
+                <h4>🎬🎶 Let's dive into the colorful world of anime together! 🔥🎭 Embracing the epic battles and strong heroines of anime.</h4>
             </div>
         </div>
     </>
