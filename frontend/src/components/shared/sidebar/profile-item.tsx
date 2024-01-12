5076f8e0b65dd4d0a1aa6ddab26704707d2ff911
tokitouq <tokitousan2005@gmail.com>
2024-01-12 07:46:50 +0530

    Fix: remove profile item transition

--- frontend/src/components/shared/sidebar/profile-item.tsx
+++ frontend/src/components/shared/sidebar/profile-item.tsx
@@ -20,8 +20,8 @@ export const ProfileItem = (props: ProfileItemProps) => {
 	return (
 		<A
 			href={`/@${username()}`}
-			class="flex w-full select-none items-center gap-[1vw] rounded-[0.75vw] px-[1vw] py-[0.75vw] transition-colors"
-			activeClass="bg-blue-500 hover:bg-blue-500 before:absolute before:left-0 before:w-[0.35vw] before:h-[2.5vw] before:bg-white before:rounded-full before:rounded-l-none"
+			class="flex w-full select-none items-center gap-[1vw] rounded-[0.75vw] px-[1vw] py-[0.75vw]"
+			activeClass="bg-blue-500 hover:bg-blue-500 before:absolute before:left-[1vw] before:w-[0.35vw] before:h-[2.5vw] before:bg-blue-50 before:rounded-full before:rounded-l-none"
 		>
 			<img
 				class="w-[3.5vw] rounded-full"
