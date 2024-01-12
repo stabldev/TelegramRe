5cbf655545bec0ec578a26db0909f439bc722abf
tokitouq <tokitousan2005@gmail.com>
2024-01-12 20:15:53 +0530

    Feat: format codes

--- frontend/src/routes/user/sign-in.tsx
+++ frontend/src/routes/user/sign-in.tsx
@@ -10,28 +10,35 @@ export default function SignIn() {
 	};
 
 	return (
-		<AuthLayout class="md:gap-[2vw] md:w-[25vw]">
+		<AuthLayout class="md:w-[25vw] md:gap-[2vw]">
 			<img
 				src="/favicon.ico"
 				class="md:size-[10vw]"
 			/>
 			<div class="flex flex-col md:gap-[0.5vw]">
-				<h2 class="text-stone-50 md:text-[1.75vw] font-semibold">Sign in to Telegram RE</h2>
-				<span class="text-stone-400 max-w-[17vw] flex self-center md:text-[1.05vw]">Sign in via Google or please enter your Email address to get OTP</span>
+				<h2 class="font-semibold text-stone-50 md:text-[1.75vw]">Sign in to Telegram RE</h2>
+				<span class="flex max-w-[17vw] self-center text-stone-400 md:text-[1.05vw]">Sign in via Google or please enter your Email address to get OTP</span>
 			</div>
-			<form onSubmit={handleFormSubmit} class="w-full flex flex-col md:gap-[1vw]">
+			<form
+				onSubmit={handleFormSubmit}
+				class="flex w-full flex-col md:gap-[1vw]"
+			>
 				<input
 					name="email"
 					placeholder="Email address"
-					class="w-full bg-transparent border-stone-700 md:border-[0.1vw] md:p-[0.75vw] md:rounded-[0.65vw] text-stone-50 md:text-[1.1vw]"
+					class="w-full border-stone-700 bg-transparent text-stone-50 md:rounded-[0.65vw] md:border-[0.1vw] md:p-[0.75vw] md:text-[1.1vw]"
 				/>
-				<div class="text-stone-50 md:text-[1vw] flex items-center md:gap-[1vw]">
-					<input name="keep-me" checked id="keep-me" type="checkbox" class="md:size-[1vw]" />
+				<div class="flex items-center text-stone-50 md:gap-[1vw] md:text-[1vw]">
+					<input
+						name="keep-me"
+						checked
+						id="keep-me"
+						type="checkbox"
+						class="md:size-[1vw]"
+					/>
 					<label for="keep-me">Keep me signed in</label>
 				</div>
-				<button class="uppercase bg-blue-600 text-white leading-none md:text-[1.1vw] md:p-[1vw] md:rounded-[0.65vw] font-medium">
-					sign in
-				</button>
+				<button class="bg-blue-600 font-medium uppercase leading-none text-white md:rounded-[0.65vw] md:p-[1vw] md:text-[1.1vw]">sign in</button>
 				<span class="text-stone-400 md:text-[0.9vw]">Note: If this email doesn't exists, we'll create a new user with this email.</span>
 			</form>
 		</AuthLayout>
