007ee8e2dffd868c2a14bcdd328eda1a30e4238a
tokitouq <tokitousan2005@gmail.com>
2024-01-12 20:15:07 +0530

    Feat: add form submit events

--- frontend/src/routes/user/sign-in.tsx
+++ frontend/src/routes/user/sign-in.tsx
@@ -19,13 +19,14 @@ export default function SignIn() {
 				<h2 class="text-stone-50 md:text-[1.75vw] font-semibold">Sign in to Telegram RE</h2>
 				<span class="text-stone-400 max-w-[17vw] flex self-center md:text-[1.05vw]">Sign in via Google or please enter your Email address to get OTP</span>
 			</div>
-			<form class="w-full flex flex-col md:gap-[1vw]">
+			<form onSubmit={handleFormSubmit} class="w-full flex flex-col md:gap-[1vw]">
 				<input
+					name="email"
 					placeholder="Email address"
 					class="w-full bg-transparent border-stone-700 md:border-[0.1vw] md:p-[0.75vw] md:rounded-[0.65vw] text-stone-50 md:text-[1.1vw]"
 				/>
 				<div class="text-stone-50 md:text-[1vw] flex items-center md:gap-[1vw]">
-					<input checked id="keep-me" type="checkbox" class="md:size-[1vw]" />
+					<input name="keep-me" checked id="keep-me" type="checkbox" class="md:size-[1vw]" />
 					<label for="keep-me">Keep me signed in</label>
 				</div>
 				<button class="uppercase bg-blue-600 text-white leading-none md:text-[1.1vw] md:p-[1vw] md:rounded-[0.65vw] font-medium">
