3f064fe354621f4d973790a09486d73f4895dfa1
tokitouq <tokitousan2005@gmail.com>
2024-01-12 20:12:38 +0530

    Fix: signin page

--- frontend/src/routes/user/sign-in.tsx
+++ frontend/src/routes/user/sign-in.tsx
@@ -10,91 +10,28 @@ export default function SignIn() {
 	};
 
 	return (
-		<AuthLayout class="md:gap-[3vw]">
+		<AuthLayout class="md:gap-[2vw] md:w-[25vw]">
+			<img
+				src="/favicon.ico"
+				class="md:size-[10vw]"
+			/>
 			<div class="flex flex-col md:gap-[0.5vw]">
-				<h2 class="font-bold md:text-[2vw]">Welcome to Telegram-re</h2>
-				<h4 class="opacity-75 md:text-[1.2vw]">A Telegram inspired web messaging experience</h4>
+				<h2 class="text-stone-50 md:text-[1.75vw] font-semibold">Sign in to Telegram RE</h2>
+				<span class="text-stone-400 max-w-[17vw] flex self-center md:text-[1.05vw]">Sign in via Google or please enter your Email address to get OTP</span>
 			</div>
-			<form
-				onSubmit={handleFormSubmit}
-				class="flex w-full flex-col md:gap-[1vw]"
-			>
-				<div class="flex flex-col md:gap-[0.25vw]">
-					<label
-						class="font-medium opacity-50 md:text-[1vw]"
-						for="username"
-					>
-						Username:
-					</label>
-					<input
-						autofocus
-						type="text"
-						id="username"
-						name="username"
-						placeholder="eg: forger"
-						class="border-stone-400 bg-transparent font-medium transition-colors focus:border-stone-900 md:border-b-[0.15vw] md:py-[0.5vw] md:text-[1.1vw]"
-					/>
-				</div>
-				<div class="flex flex-col md:gap-[0.25vw]">
-					<label
-						class="font-medium opacity-50 md:text-[1vw]"
-						for="password"
-					>
-						Password:
-					</label>
-					<input
-						type="text"
-						id="password"
-						name="password"
-						placeholder="eg: forger-****"
-						class="border-stone-400 bg-transparent font-medium transition-colors focus:border-stone-900 md:border-b-[0.15vw] md:py-[0.5vw] md:text-[1.1vw]"
-					/>
-				</div>
-				<div class="flex flex-col md:gap-[0.25vw]">
-					<label
-						class="font-medium opacity-50 md:text-[1vw]"
-						for="confirm-password"
-					>
-						Confirm Password:
-					</label>
-					<input
-						type="text"
-						id="confirm-password"
-						name="confirm-password"
-						placeholder="eg: forger-****"
-						class="border-stone-400 bg-transparent font-medium transition-colors focus:border-stone-900 md:border-b-[0.15vw] md:py-[0.5vw] md:text-[1.1vw]"
-					/>
-				</div>
-				<div class="flex items-center md:gap-[0.5vw]">
-					<input
-						checked
-						type="checkbox"
-						id="remember-me"
-						name="remember-me"
-						class="accent-stone-900"
-					/>
-					<label
-						for="remember-me"
-						class="opacity-75 md:text-[1vw]"
-					>
-						Agree with Terms and conditions
-					</label>
-				</div>
+			<form class="w-full flex flex-col md:gap-[1vw]">
 				<input
-					type="submit"
-					value="Sign in"
-					class="cursor-pointer bg-stone-900 font-medium text-white md:rounded-full md:py-[1vw] md:text-[1.1vw]"
+					placeholder="Email address"
+					class="w-full bg-transparent border-stone-700 md:border-[0.1vw] md:p-[0.75vw] md:rounded-[0.65vw] text-stone-50 md:text-[1.1vw]"
 				/>
-				<span class="self-center md:text-[1.1vw]">
-					Don't have an account?&nbsp;
-					<A
-						href="/user/sign-up/"
-						class="font-medium underline"
-					>
-						Sign up
-					</A>
-					&nbsp; now!
-				</span>
+				<div class="text-stone-50 md:text-[1vw] flex items-center md:gap-[1vw]">
+					<input checked id="keep-me" type="checkbox" class="md:size-[1vw]" />
+					<label for="keep-me">Keep me signed in</label>
+				</div>
+				<button class="uppercase bg-blue-600 text-white leading-none md:text-[1.1vw] md:p-[1vw] md:rounded-[0.65vw] font-medium">
+					sign in
+				</button>
+				<span class="text-stone-400 md:text-[0.9vw]">Note: If this email doesn't exists, we'll create a new user with this email.</span>
 			</form>
 		</AuthLayout>
 	);
