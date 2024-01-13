4510219c9a591ad342ef3ff9d6ca69df5dd34292
tokitouq <tokitousan2005@gmail.com>
2024-01-13 07:51:07 +0530

    Feat: add maxLength

--- frontend/src/components/pages/user/sign-in/otp-form.tsx
+++ frontend/src/components/pages/user/sign-in/otp-form.tsx
@@ -44,6 +44,7 @@ const OtpForm = (props: Props) => {
                     onKeyDown={handleKeyDown}
 					name="code"
 					placeholder="Code"
+                    maxLength={5}
 					class="w-full border-stone-700 bg-transparent text-stone-50 md:rounded-[0.65vw] md:border-[0.1vw] md:p-[0.75vw] md:text-[1.1vw]"
 				/>
 				<button class="bg-blue-600 font-medium uppercase leading-none text-white md:rounded-[0.65vw] md:p-[1vw] md:text-[1.1vw]">Submit</button>
