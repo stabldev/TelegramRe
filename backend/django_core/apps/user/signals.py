c68fdc7c8c9fbdd71d253d932f6cf19613c3ad7b
tokitouq <tokitousan2005@gmail.com>
2024-01-14 20:13:08 +0530

    fix: update signal for handling unique username

--- backend/django_core/apps/user/signals.py
+++ backend/django_core/apps/user/signals.py
@@ -1,10 +1,21 @@
 from django.db.models.signals import post_save
 from .models import CustomUser
 from django.dispatch import receiver
+import uuid
+from django.db import IntegrityError
 
 
 @receiver(post_save, sender=CustomUser)
 def create_username(sender, instance, created, **kwargs):
     if created:
-        instance.username = instance.email.split("@")[0]
-        instance.save()
+        base_username = instance.email.split("@")[0]
+
+        while True:
+            unique_id = str(uuid.uuid4().hex[:4])
+            instance.username = f"{base_username}-{unique_id}"
+
+            try:
+                instance.save()
+                break
+            except IntegrityError:
+                continue
