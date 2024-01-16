5f8182a6257a1b05361bddd195c776d8d8db9b9f
tokitouq <tokitousan2005@gmail.com>
2024-01-16 19:08:11 +0530

    fix: otp form

--- frontend/src/components/pages/auth/login/otp-form.tsx
+++ frontend/src/components/pages/auth/login/otp-form.tsx
@@ -15,21 +15,20 @@ const OtpForm = (props: Props) => {
 	const handleFormSubmit = (e: SubmitEvent) => {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget as HTMLFormElement);
-		const code = formData.get("code");
+		const otp = formData.get("otp");
 
-		dispatch("otpSubmit", code);
+		dispatch("otpSubmit", otp);
 	};
 
 	return (
 		<>
 			<div class="flex flex-col md:gap-[0.5vw]">
-				<h2 class="flex items-center font-semibold text-stone-50 md:gap-[0.75vw] md:text-[1.75vw]">
-					<span>test@test.com</span>
-					<button class="text-stone-400 transition-colors hover:text-stone-300">
-						<Pencil />
-					</button>
+				<h2 class="font-semibold text-stone-50 md:text-[1.75vw]">
+					Email verification.
 				</h2>
-				<span class="flex max-w-[17vw] self-center text-stone-400 md:text-[1.05vw]">We have send you a message in Email with the Code</span>
+				<span class="flex max-w-[20vw] self-center text-stone-400 md:text-[1.05vw]">
+					Please enter the OTP which we've send to your email to complete.
+				</span>
 			</div>
 			<form
 				onSubmit={handleFormSubmit}
@@ -38,8 +37,8 @@ const OtpForm = (props: Props) => {
 				<input
 					required
 					autofocus
-                    name="code"
-					placeholder="Code"
+                    name="otp"
+					placeholder="One Time Password"
 					minLength={5}
 					maxLength={5}
 					class="w-full border-stone-700 bg-transparent text-stone-50 md:rounded-[0.65vw] md:border-[0.1vw] md:p-[0.75vw] md:text-[1.1vw]"
@@ -52,7 +51,7 @@ const OtpForm = (props: Props) => {
 					<Show when={loading()}>
 						<Spinner class="md:size-[1vw]" />
 					</Show>
-					{loading() ? "please wait..." : "submit"}
+					{loading() ? "please wait..." : "verify"}
 				</button>
 			</form>
 		</>
