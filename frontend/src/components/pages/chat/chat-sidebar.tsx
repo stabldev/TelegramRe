8b16b5e86241245341dea16f8b93f5744b0305c1
tokitouq <tokitousan2005@gmail.com>
2024-01-20 10:34:33 +0530

    fix: chat sidebar

--- frontend/src/components/pages/chat/chat-sidebar.tsx
+++ frontend/src/components/pages/chat/chat-sidebar.tsx
@@ -12,26 +12,22 @@ export const ChatSidebar = () => {
 				class="h-full whitespace-nowrap border-l-[0.1vw] border-black/50 bg-stone-900 md:w-full"
 				style={{ "max-height": "calc(100vh - 3.75vw)" }}
 			>
-				<div class="flex items-center justify-between md:p-[1vw]">
-					<h3 class="flex items-center font-medium text-stone-200 md:gap-[0.75vw] md:text-[1.25vw]">
+				<div class="flex items-center justify-between md:p-3">
+					<h3 class="flex items-center font-medium text-stone-200 md:gap-3 md:text-sm">
 						Profile
-						<span class="font-normal text-stone-400 md:text-[1vw]">{params.username}</span>
+						<span class="font-normal text-stone-400">{params.username}</span>
 					</h3>
 					<button onClick={toggleShowSidebar}>
-						<Close class="text-white/50 transition-colors hover:text-white/75 md:w-[1.75vw]" />
+						<Close class="text-white/50 transition-colors hover:text-white/75 md:size-6" />
 					</button>
 				</div>
 				<img
 					src="https://pm1.aminoapps.com/8063/ff1db42bbc3a7bc249022b37125da8fa3b1e2d4br1-512-512v2_hq.jpg"
-					class="object-cover md:size-[25vw]"
+					class="object-cover md:size-auto"
 				/>
-				<div class="flex flex-col leading-none md:gap-[0.5vw] md:p-[1vw]">
-					<h3 class="font-medium text-stone-100 md:text-[1.25vw]">Anya Forger</h3>
-					<h5 class="text-stone-400 md:text-[1vw]">Joined Date: Dec 25, 2023</h5>
-				</div>
-				<div class="flex flex-col leading-none md:hidden md:gap-[0.5vw] md:p-[1vw]">
-					<h5 class="text-stone-400 md:text-[1vw]">BIO:</h5>
-					<h4>🎬🎶 Let's dive into the colorful world of anime together! 🔥🎭 Embracing the epic battles and strong heroines of anime.</h4>
+				<div class="flex flex-col leading-none md:gap-1 md:p-3">
+					<h3 class="font-medium text-stone-100 md:text-base">Anya Forger</h3>
+					<h5 class="text-stone-400 md:text-xs">Joined Date: Dec 25, 2023</h5>
 				</div>
 			</div>
 		</>
--- frontend/src/layouts/default-layout.tsx
+++ frontend/src/layouts/default-layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "~/components/shared/sidebar";
 export function DefaultLayout(props: { children?: JSX.Element }) {
 	return (
 		<main
-			class="2xl:container mx-auto grid h-screen w-screen grid-cols-[21rem_1fr]"
+			class="2xl:container mx-auto grid h-screen w-screen grid-cols-[21rem_1fr] overflow-x-hidden"
 		>
 			{/* dark overlay for background-image */}
 			<div class="absolute inset-0 -z-[9999] bg-black/95" />
