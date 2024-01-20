fd61d2f991f2bcb3830f9df8360017424064fd15
tokitouq <tokitousan2005@gmail.com>
2024-01-20 14:03:27 +0530

    feat: add new hook

--- frontend/src/layouts/auth-layout.tsx
+++ frontend/src/layouts/auth-layout.tsx
@@ -1,10 +1,18 @@
-import { JSX } from "solid-js";
+import { JSX, Show } from "solid-js";
+import { Navigate } from "solid-start";
+import { useAuth } from "~/context/auth";
 import { cn } from "~/functions/cn";
 
 export function AuthLayout(props: { children?: JSX.Element; class?: string }) {
+	const { isAuthenticated } = useAuth();
 	return (
-		<main class="relative grid h-screen w-screen place-items-center bg-stone-900">
-			<div class={cn(props.class, "flex flex-col items-center text-center")}>{props.children}</div>
-		</main>
+		<Show
+			when={!isAuthenticated()}
+			fallback={ <Navigate href={"/"} /> }
+		>
+			<main class="relative grid h-screen w-screen place-items-center bg-stone-900">
+				<div class={cn(props.class, "flex flex-col items-center text-center")}>{props.children}</div>
+			</main>
+		</Show>
 	);
 }
--- frontend/src/layouts/default-layout.tsx
+++ frontend/src/layouts/default-layout.tsx
@@ -1,16 +1,25 @@
-import { JSX } from "solid-js";
+import { JSX, Show } from "solid-js";
+import { Navigate } from "solid-start";
 import Sidebar from "~/components/shared/sidebar";
+import { useAuth } from "~/context/auth";
 
 export function DefaultLayout(props: { children?: JSX.Element }) {
-	return (
-		<main
-			class="2xl:container mx-auto grid h-screen w-screen grid-cols-[21rem_1fr] overflow-x-hidden"
-		>
-			{/* dark overlay for background-image */}
-			<div class="absolute inset-0 -z-[9999] bg-black/95" />
+	const { isAuthenticated } = useAuth();
 
-			<Sidebar />
-			{props.children}
-		</main>
+	return (
+		<Show
+			when={isAuthenticated()}
+			fallback={ <Navigate href={"/auth/login"} /> }
+		>
+			<main
+				class="2xl:container mx-auto grid h-screen w-screen grid-cols-[21rem_1fr] overflow-x-hidden"
+			>
+				{/* dark overlay for background-image */}
+				<div class="absolute inset-0 -z-[9999] bg-black/95" />
+
+				<Sidebar />
+				{props.children}
+			</main>
+		</Show>
 	);
 }
