1568e40d2241aff311fb2de3571523d2f2116673
tokitouq <tokitousan2005@gmail.com>
2024-01-20 10:22:07 +0530

    fix: chat bubble design and measures

--- frontend/src/components/pages/chat/chat-area.tsx
+++ frontend/src/components/pages/chat/chat-area.tsx
@@ -20,33 +20,30 @@ export const ChatArea = (props: Props) => {
 		<div class="relative flex">
 			<div
 				ref={props.ref}
-				class="mx-auto flex w-full flex-col gap-[0.5vw] self-end overflow-y-scroll px-[1vw] pb-[4.5vw] pt-[5vw] [scrollbar-color:_rgba(255,255,255,0.1)_transparent] [scrollbar-width:_thin] md:w-[50vw] md:min-w-[50vw]"
-				style={{ "max-height": "calc(100vh - 3.75vw)" }}
+				class="mx-auto flex w-full flex-col gap-2 self-end overflow-y-scroll px-2 pb-14 pt-10 [scrollbar-color:_rgba(255,255,255,0.1)_transparent] [scrollbar-width:_thin] md:w-[43rem] md:min-w-[43rem]"
+				style={{ "max-height": "calc(100vh - 3rem)" }}
 			>
 				<For each={groupChatBySender(props.chat)}>
 					{(group) => (
 						<div
-							class="relative flex items-end gap-[0.65vw]"
-							classList={{ "self-end flex-row-reverse": group.sender.username === user()!.username }}
+							class="relative flex items-end gap-2"
+							classList={{ "self-end flex-row-reverse": group.sender.username === "tokitouq" }}
 						>
 							<img
 								hidden
 								src={group.sender.image}
 								alt="anya-forger"
-								class="sticky bottom-0 w-[2.15vw] select-none rounded-full"
+								class="sticky bottom-0 size-4 select-none rounded-full"
 							/>
 							<div
-								class="flex flex-col gap-[0.15vw]"
-								classList={{ "items-end": group.sender.username === user()!.username }}
+								class="flex flex-col gap-0.5"
+								classList={{ "items-end": group.sender.username === "tokitouq" }}
 							>
 								<For each={group.chats}>
 									{(message, i) => (
 										<ChatBubble
 											message={message}
-											self={group.sender.username === user()!.username}
-											lastMessage={message.username !== group.chats[i() + 1]?.username && group.chats.length !== 1}
-											middleMessage={message.username === group.chats[i() - 1]?.username && message.username === group.chats[i() + 1]?.username}
-											firstMessage={message.username !== group.chats[i() - 1]?.username && message.username === group.chats[i() + 1]?.username}
+											self={group.sender.username === "tokitouq"}
 										/>
 									)}
 								</For>
