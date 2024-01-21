50ea3e942a0fe1fbb8eac49324efc3f13dc5463e
tokitouq <tokitousan2005@gmail.com>
2024-01-21 16:02:49 +0530

    fix: get inbox and render logics

--- frontend/src/components/shared/sidebar/index.tsx
+++ frontend/src/components/shared/sidebar/index.tsx
@@ -1,31 +1,11 @@
 import { Component, For, createEffect, createResource } from "solid-js";
-import { ProfileItemProps } from "../../../types/profile-item.types";
 import { SearchHeader } from "./search-header";
 import Pencil from "~/icons/pencil";
 import { ProfileItem } from "./profile-item";
 import { API_URL } from "~/config";
 import { InboxItem } from "~/types/inbox.types";
 
-const profile_mapping: ProfileItemProps[] = [
-	{
-		name: "Anya Forger",
-		username: "anya-forger",
-		image: "https://pm1.aminoapps.com/8063/ff1db42bbc3a7bc249022b37125da8fa3b1e2d4br1-512-512v2_hq.jpg",
-		message: "Hi wassup! I've something to tell you, so please reply when you're free",
-		timestamp: "2023-10-02T05:21:26Z",
-		new_message: false
-	},
-	{
-		name: "Toshinou Kyouko",
-		username: "kyouko",
-		image: "https://pbs.twimg.com/media/D2v3DBuXQAAMFIb.jpg",
-		message: "Goodbye, Tokito! Have a great day!",
-		timestamp: "2023-10-01T05:21:26Z",
-		new_message: false
-	}
-];
-
-async function fetchInbox() {
+async function fetchInbox(): Promise<InboxItem[]> {
 	const res = await fetch(`${API_URL}/inbox/`, {
 		credentials: "include",
 	});
@@ -44,7 +24,7 @@ const Sidebar: Component = () => {
 		<div class="relative grid h-screen w-full grid-rows-[min-content_1fr] border-r border-black/50 bg-stone-900">
 			<SearchHeader />
 			<div class="overflow-y-scroll px-3 [scrollbar-width:_thin]">
-				<For each={profile_mapping}>{(profile) => <ProfileItem {...profile} />}</For>
+				<For each={inbox()}>{(item) => <ProfileItem {...item} />}</For>
 			</div>
 			<button class="absolute bottom-3 right-3 rounded-full bg-blue-500 p-4">
 				<Pencil class="text-xl text-white" />
--- frontend/src/components/shared/sidebar/profile-item.tsx
+++ frontend/src/components/shared/sidebar/profile-item.tsx
@@ -1,32 +1,36 @@
 import { A, useParams } from "solid-start";
 import { destructure } from "@solid-primitives/destructure";
 import { FormatDate } from "~/functions/format-date";
-import { ProfileItemProps } from "~/types/profile-item";
 import { createEffect, createSignal } from "solid-js";
 import { get_username } from "~/functions/get-username";
+import { InboxItem } from "~/types/inbox.types";
+import { useAuth } from "~/context/auth";
 
-export const ProfileItem = (props: ProfileItemProps) => {
+export const ProfileItem = (props: InboxItem) => {
+	const { user } = useAuth();
 	const [isActive, setIsActive] = createSignal(false);
 	const params = useParams<{ username: string }>();
 
-	const { image, message, name, timestamp, username } = destructure(props);
-	const formated_timestamp = new FormatDate(timestamp()).format_to_relative_time;
+	const { message, sender, reciever, date } = destructure(props);
+
+	const chat_user = sender().id !== user()?.id ? sender : reciever;
+	const formated_timestamp = new FormatDate(date()).format_to_relative_time;
 
 	createEffect(() => {
 		if (!params.username) return;
-		setIsActive(get_username(params.username) === username());
+		setIsActive(get_username(params.username) === chat_user().username);
 	}, [params.username]);
 
 	return (
 		<A
-			href={`/@${username()}`}
+			href={`/@${chat_user().username}`}
 			class="flex w-full select-none items-center gap-3 rounded-lg px-3 py-2"
 			activeClass="bg-blue-500 hover:bg-blue-500 before:absolute before:left-3 before:w-1 before:h-8 before:bg-blue-50 before:rounded-full before:rounded-l-none"
 		>
 			<img
 				class="size-12 rounded-full"
-				src={image()}
-				alt={name()}
+				src={chat_user().avatar ?? ""}
+				alt={chat_user().username}
 			/>
 			<div
 				class="flex w-full flex-col"
@@ -36,7 +40,9 @@ export const ProfileItem = (props: ProfileItemProps) => {
 				}}
 			>
 				<div class="flex items-center justify-between">
-					<span class="text-sm font-medium text-white">{name()}</span>
+					<span class="text-sm font-medium text-white">
+						{chat_user().first_name + " " + chat_user().last_name}
+					</span>
 					<span class="text-xs uppercase">{formated_timestamp}</span>
 				</div>
 				<div>
