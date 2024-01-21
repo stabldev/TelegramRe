8192011c34ce7fa289b71860480445373a277d24
tokitouq <tokitousan2005@gmail.com>
2024-01-21 18:50:08 +0530

    chore: remove dead codes

--- frontend/src/components/shared/sidebar/index.tsx
+++ frontend/src/components/shared/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Component, For, createEffect, createResource } from "solid-js";
+import { Component, For, createResource } from "solid-js";
 import { SearchHeader } from "./search-header";
 import Pencil from "~/icons/pencil";
 import { ProfileItem } from "./profile-item";
@@ -16,15 +16,11 @@ async function fetchInbox(): Promise<InboxItem[]> {
 const Sidebar: Component = () => {
 	const [inbox] = createResource<InboxItem[]>(fetchInbox);
 
-	createEffect(() => {
-		console.log(inbox());
-	});
-
 	return (
 		<div class="relative grid h-screen w-full grid-rows-[min-content_1fr] border-r border-black/50 bg-stone-900">
 			<SearchHeader />
 			<div class="overflow-y-scroll px-3 [scrollbar-width:_thin]">
-				<For each={inbox()}>{(item) => <ProfileItem {...item} />}</For>
+				<For each={inbox()}>{(chat) => <ProfileItem {...chat} />}</For>
 			</div>
 			<button class="absolute bottom-3 right-3 rounded-full bg-blue-500 p-4">
 				<Pencil class="text-xl text-white" />
