e78af7b4053397cd2ad42a88f90a1c9b7d7df4d5
tokitouq <tokitousan2005@gmail.com>
2024-01-21 23:59:15 +0530

    fix: chat type

--- frontend/src/types/chat.types.ts
+++ frontend/src/types/chat.types.ts
@@ -6,3 +6,12 @@ export type ChatProps = {
 	time: string;
 	status: "seen" | "send" | "sending";
 };
+
+export type ChatMessage = {
+	id: number;
+	sender: number;
+	reciever: number;
+	message: string;
+	is_read: boolean;
+	date: string;
+}
