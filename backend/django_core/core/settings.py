0ec78aa2601654e62c6eecf766444dc89289d7c0
tokitouq <tokitousan2005@gmail.com>
2024-01-31 21:19:51 +0530

    feat: add redis channel layers

--- backend/django_core/core/settings.py
+++ backend/django_core/core/settings.py
@@ -119,9 +119,16 @@ DATABASES = {
 }
 
 # Channel Layers
-# TODO: use redis later
+# https://channels.readthedocs.io/en/latest/topics/channel_layers.html#redis-channel-layer
 
-CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 
 # Password validation
