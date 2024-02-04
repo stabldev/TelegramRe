6e5c274574336169e306003a47b49ec9e7e04c68
tokitouq <tokitousan2005@gmail.com>
2024-02-04 21:36:43 +0530

    feat: split sidebar into chatbar

---
+++ frontend/src/components/shared/sidebar/chat-bar/index.tsx
@@ -0,0 +1,31 @@
+import { destructure } from "@solid-primitives/destructure";
+import { SearchHeader } from "./search-header";
+import { For } from "solid-js";
+import { useChat } from "~/context/chat";
+import { ProfileItem } from "./profile-item";
+import Pencil from "~/icons/pencil";
+
+type Props = {
+    toggleView: () => void;
+    isLoading: boolean;
+
+};
+
+export const ChatBar = (props: Props) => {
+    const { chatRooms } = useChat();
+    const { toggleView } = destructure(props);
+
+    return (
+        <>
+            <SearchHeader toggleView={toggleView} />
+            <div class="overflow-y-scroll px-3 [scrollbar-width:_thin]">
+                <For each={chatRooms()?.sort((a, b) => b.message.id - a.message.id)}>
+                    {(room) => <ProfileItem {...room} />}
+                </For>
+            </div>
+            <button class="absolute bottom-3 right-3 rounded-full bg-blue-500 p-4">
+                <Pencil class="text-xl text-white" />
+            </button>
+        </>
+    );
+};
