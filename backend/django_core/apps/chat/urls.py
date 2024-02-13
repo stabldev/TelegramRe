fac1fad3b4b98481abed77717a148e6cbef0eccc
tokitouq <tokitousan2005@gmail.com>
2024-02-13 21:22:49 +0530

    prevent: black formating on urls

--- backend/django_core/apps/chat/urls.py
+++ backend/django_core/apps/chat/urls.py
@@ -2,23 +2,14 @@ from django.urls import path, include
 
 from . import views
 
+# fmt: off
 urlpatterns = [
-    path(
-        "chat-rooms/",
-        include(
-            [
-                path("", views.ChatRoomListView.as_view()),
-                path(
-                    "<str:room_id>/",
-                    include(
-                        [
-                            path("", views.ChatMessageView.as_view()),
-                            path("read-all/", views.ReadRoomChatMessages.as_view()),
-                        ]
-                    ),
-                ),
-            ]
-        ),
-    ),
-    path("online-users/", views.OnlineUsersView.as_view()),
+    path("chat-rooms/", include([
+        path("", views.ChatRoomListView.as_view()),
+        path("<str:room_id>/", include([
+            path("", views.ChatMessageView.as_view()),
+            path("read-all/", views.ReadRoomChatMessages.as_view()),
+        ])),
+    ])),
 ]
+# fmt: on
