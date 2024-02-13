c0527b869cbdbd9346afd4830128e5a46021aa23
tokitouq <tokitousan2005@gmail.com>
2024-02-13 12:42:42 +0530

    feat: add gif and image seperate selector

--- frontend/src/components/pages/chat/chat-input.tsx
+++ frontend/src/components/pages/chat/chat-input.tsx
@@ -8,6 +8,8 @@ import { ChatFileModal } from "~/components/shared/chat/chat-file-modal";
 import { useChat } from "~/context/chat";
 import ApiEndpoints from "~/connections/api/api-endpoints";
 import { useAuth } from "~/context/auth";
+import Photo from "~/icons/photo";
+import Gif from "~/icons/gif";
 
 
 export const ChatInput = () => {
@@ -16,6 +18,7 @@ export const ChatInput = () => {
 
 	const [message, setMessage] = createSignal("");
 	const [showFileModel, setShowFileModel] = createSignal(false);
+	const [showFileTypeSelect, setShowFileTypeSelect] = createSignal(false);
 	const [file, setFile] = createSignal<File>();
 
 	let inputRef: HTMLTextAreaElement;
@@ -95,6 +98,10 @@ export const ChatInput = () => {
 		setShowFileModel(true);
 	};
 
+	const handleToggleShowFileTypeSelect = () => {
+		setShowFileTypeSelect((prev) => !prev);
+	};
+
 	return (
 		<>
 			<Show when={showFileModel()}>
@@ -108,13 +115,34 @@ export const ChatInput = () => {
 				onSubmit={handleSubmit}
 				class="absolute bottom-0 flex w-full items-end gap-3 bg-stone-900 p-3"
 			>
-				<input type="file" id="file-input" class="hidden" onChange={handleFileChange} />
-				<label
-					for="file-input"
-					class="cursor-pointer text-2xl text-white/50 transition-colors hover:text-white/75"
-				>
-					<Clip />
-				</label>
+				<div class="flex relative">
+					<Show when={!showFileTypeSelect()}>
+						<div class="absolute bottom-11 z-50 w-max select-none overflow-hidden rounded-xl bg-stone-900">
+							<label
+								for="image-file-input"
+								class="cursor-pointer grid grid-cols-12 gap-2 px-3 py-2 text-start text-stone-300 transition-colors hover:bg-stone-800 hover:text-stone-100"
+							>
+								<Photo class="col-span-2 size-full" />
+								<span class="col-span-10 text-sm font-medium">Send Photo</span>
+							</label>
+							<label
+								for="gif-file-input"
+								class="cursor-pointer grid grid-cols-12 gap-2 px-3 py-2 text-start text-stone-300 transition-colors hover:bg-stone-800 hover:text-stone-100"
+							>
+								<Gif class="col-span-2 size-full" />
+								<span class="col-span-10 text-sm font-medium">Send GIF</span>
+							</label>
+						</div>
+					</Show>
+					<input type="file" id="image-file-input" accept=".png,.jpg,.jpeg" class="hidden" onChange={handleFileChange} />
+					<input type="file" id="gif-file-input" accept=".gif" class="hidden" onChange={handleFileChange} />
+					<button
+						onClick={handleToggleShowFileTypeSelect}
+						class="cursor-pointer text-2xl text-white/50 transition-colors hover:text-white/75"
+					>
+						<Clip />
+					</button>
+				</div>
 				<TextareaAutosize
 					ref={(ref) => (inputRef = ref)}
 					value={message()}
