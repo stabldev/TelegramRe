4ff9ca57cd6af6f95e99274dc75c6b3b1302bc86
tokitouq <tokitousan2005@gmail.com>
2024-02-15 19:43:53 +0530

    feat: add reactivity to chatarea  props

--- frontend/src/components/pages/chat/chat-area.tsx
+++ frontend/src/components/pages/chat/chat-area.tsx
@@ -3,6 +3,7 @@ import { groupChatBySender } from "~/functions/group-chat";
 import { ChatBubble } from "./chat-bubble";
 import { useAuth } from "~/context/auth";
 import { ChatMessage } from "~/types/chat.types";
+import { destructure } from "@solid-primitives/destructure";
 
 interface Props {
 	chat: ChatMessage[];
@@ -11,42 +12,45 @@ interface Props {
 
 export const ChatArea = (props: Props) => {
 	const { user } = useAuth();
+	const { ref, chat } = destructure(props);
 
 	return (
-		<div
-			ref={props.ref}
-			class="mx-auto flex w-full flex-col gap-2 self-end overflow-y-scroll px-2 pb-14 pt-10 [scrollbar-color:_rgba(255,255,255,0.1)_transparent] [scrollbar-width:none] md:w-[43rem] md:min-w-[43rem]"
-			style={{ "max-height": "calc(100vh - 3rem)" }}
-		>
-			<For each={groupChatBySender(props.chat)}>
-				{(group) => (
-					<div
-						class="relative flex items-end gap-2"
-						classList={{ "self-end flex-row-reverse": group.sender === user()?.id }}
-					>
+		<>
+			<div
+				ref={ref()}
+				class="mx-auto flex w-full flex-col gap-2 self-end overflow-y-scroll px-2 pb-14 pt-10 [scrollbar-color:_rgba(255,255,255,0.1)_transparent] [scrollbar-width:none] md:w-[43rem] md:min-w-[43rem] relative"
+				style={{ "max-height": "calc(100vh - 3rem)" }}
+			>
+				<For each={groupChatBySender(chat())}>
+					{(group) => (
 						<div
-							class="flex flex-col gap-0.5"
-							classList={{ "items-end": group.sender === user()?.id }}
+							class="relative flex items-end gap-2"
+							classList={{ "self-end flex-row-reverse": group.sender === user()?.id }}
 						>
-							<For each={group.chats}>
-								{(message, idx) => {
-									let isFirstMsg = group.chats[idx() - 1]?.sender !== message.sender;
-									let isLastMsg = group.chats[idx() + 1]?.sender !== message.sender;
+							<div
+								class="flex flex-col gap-0.5"
+								classList={{ "items-end": group.sender === user()?.id }}
+							>
+								<For each={group.chats}>
+									{(message, idx) => {
+										let isFirstMsg = group.chats[idx() - 1]?.sender !== message.sender;
+										let isLastMsg = group.chats[idx() + 1]?.sender !== message.sender;
 
-									return (
-										<ChatBubble
-											message={message}
-											self={message.sender === user()?.id}
-											firstMsg={isFirstMsg}
-											lastMsg={isLastMsg}
-										/>
-									)
-								}}
-							</For>
+										return (
+											<ChatBubble
+												message={message}
+												self={message.sender === user()?.id}
+												firstMsg={isFirstMsg}
+												lastMsg={isLastMsg}
+											/>
+										)
+									}}
+								</For>
+							</div>
 						</div>
-					</div>
-				)}
-			</For>
-		</div>
+					)}
+				</For>
+			</div>
+		</>
 	);
 };
