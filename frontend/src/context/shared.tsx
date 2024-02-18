f2e5838daf9a76ecb9abea8d0a88b1cf02c14427
tokitouq <tokitousan2005@gmail.com>
2024-02-18 16:54:05 +0530

    remove: editMessage signal from context

--- frontend/src/context/shared.tsx
+++ frontend/src/context/shared.tsx
@@ -1,18 +1,14 @@
 import { Accessor, JSX, Setter, createContext, createSignal, useContext } from "solid-js";
-import { ChatMessage } from "~/types/chat.types";
 
 type SharedStore = {
 	showSidebar: Accessor<boolean>;
 	toggleShowSidebar: () => void;
-	editMessage: Accessor<ChatMessage | undefined>;
-	setEditMessage: Setter<ChatMessage | undefined>;
 };
 
 const SharedContext = createContext<SharedStore>();
 
 export function SharedProvider(props: { children?: JSX.Element }) {
 	const [showSidebar, setShowSidebar] = createSignal(true);
-	const [editMessage, setEditMessage] = createSignal<ChatMessage>();
 
 	// Functions
 	const toggleShowSidebar = () => setShowSidebar((prev) => !prev);
@@ -20,8 +16,6 @@ export function SharedProvider(props: { children?: JSX.Element }) {
 	const context_value: SharedStore = {
 		showSidebar: showSidebar,
 		toggleShowSidebar: toggleShowSidebar,
-		editMessage: editMessage,
-		setEditMessage: setEditMessage,
 	};
 
 	return <SharedContext.Provider value={context_value}>{props.children}</SharedContext.Provider>;
