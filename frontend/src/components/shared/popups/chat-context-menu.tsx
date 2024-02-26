a9027195584917dc8c22eb8914eed91c2a879601
tokitouq <tokitousan2005@gmail.com>
2024-02-26 14:51:21 +0530

    feat: fix chat context menu

--- frontend/src/components/shared/popups/chat-context-menu.tsx
+++ frontend/src/components/shared/popups/chat-context-menu.tsx
@@ -1,4 +1,3 @@
-import { destructure } from "@solid-primitives/destructure";
 import { createEventDispatcher } from "@solid-primitives/event-dispatcher";
 import { Accessor, Show, createSignal, onCleanup, onMount } from "solid-js";
 import { useShared } from "~/context/shared";
@@ -39,10 +38,12 @@ export const ChatContextMenu = (props: Props) => {
         setY((prev) => prev > window.innerHeight - rect.height ? prev - rect.height : prev);
 
 		document.addEventListener("click", handleOutsideClick);
+        document.addEventListener("contextmenu", handleOutsideClick);
 	});
 
 	onCleanup(() => {
 		document.removeEventListener("click", handleOutsideClick);
+        document.removeEventListener("contextmenu", handleOutsideClick);
 	});
 
     // Functions
@@ -58,39 +59,41 @@ export const ChatContextMenu = (props: Props) => {
 
     return (
         <div
-			ref={ref!}
-			class="absolute z-50 w-max select-none overflow-hidden rounded-lg bg-stone-900 p-1"
+            ref={ref!}
+            class="dropdown absolute"
             style={{
                 top: `${y()}px`,
                 left: `${x()}px`,
             }}
-		>
-            <Show when={props.message().is_read && props.self()}>
+        >
+            <div class="btn btn-base-300 z-50 flex-col gap-0 w-max h-max min-h-max select-none overflow-hidden rounded-xl border-none shadow p-1">
+                <Show when={props.message().is_read && props.self()}>
+                    <div
+                        class="grid grid-cols-12 gap-2 px-3 py-1.5 text-start text-accent"
+                    >
+                        <Tick variant="double" class="col-span-2 size-full" />
+                        <span class="col-span-10 text-sm font-medium">Seen {formatedDate}</span>
+                    </div>
+                </Show>
+                <Show when={props.self()}>
+                    <div
+                        onClick={handleEdit}
+                        class="grid cursor-pointer grid-cols-12 gap-2 px-3 py-1.5 rounded-lg text-start text-accent hover:bg-base-100"
+                    >
+                        <Pencil class="col-span-2 size-full" />
+                        <span class="col-span-10 text-sm font-medium">Edit</span>
+                    </div>
+                </Show>
                 <div
-                    class="grid grid-cols-12 gap-2 px-3 py-1.5 text-start text-stone-300"
+                    onClick={handleCopy}
+                    class="grid cursor-pointer grid-cols-12 gap-2 px-3 py-1.5 rounded-lg text-start text-accent hover:bg-base-100"
                 >
-                    <Tick variant="double" class="col-span-2 size-full" />
-                    <span class="col-span-10 text-sm font-medium">Seen {formatedDate}</span>
+                    <Copy class="col-span-2 size-full" />
+                    <span class="col-span-10 text-sm font-medium">
+                        {copied() ? "Copied" : "Copy"}
+                    </span>
                 </div>
-            </Show>
-			<Show when={props.self()}>
-                <div
-                    onClick={handleEdit}
-                    class="grid cursor-pointer grid-cols-12 gap-2 px-3 py-1.5 rounded-md text-start text-stone-300 hover:bg-stone-800 hover:text-stone-100"
-                >
-                    <Pencil class="col-span-2 size-full" />
-                    <span class="col-span-10 text-sm font-medium">Edit</span>
-                </div>
-            </Show>
-            <div
-                onClick={handleCopy}
-                class="grid cursor-pointer grid-cols-12 gap-2 px-3 py-1.5 rounded-md text-start text-stone-300 hover:bg-stone-800 hover:text-stone-100"
-            >
-                <Copy class="col-span-2 size-full" />
-                <span class="col-span-10 text-sm font-medium">
-                    {copied() ? "Copied" : "Copy"}
-                </span>
             </div>
-		</div>
+        </div>
     )
 };
