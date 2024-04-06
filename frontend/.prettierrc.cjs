ee80741302f44b33bf78ac846f6121f797f51977
tokitouq <tokitousan2005@gmail.com>
2024-04-06 21:33:17 +0530

    config: eslint and prettier

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: false,
+	trailingComma: "none",
+	bracketSameLine: false,
+	singleAttributePerLine: true,
+	htmlWhitespaceSensitivity: "ignore",
+	arrowParens: "always",
+	printWidth: 400,
+	plugins: ["prettier-plugin-tailwindcss"]
+};
