970501e7e78c7c05ac70431ee72162db60bb302b
tokitouq <suneeth.since2005@gmail.com>
2024-04-15 18:04:06 +0530

    feat: config newly added pallette in tailwind config file

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-	theme: {},
+	theme: {
+        colors: {
+            "primary": "var(--primary)",
+            "base-100": "var(--base-100)",
+            "base-200": "var(--base-200)",
+            "base-300": "var(--base-300)",
+            "neutral-100": "var(--neutral-100)",
+            "neutral-200": "var(--neutral-200)",
+            "accent": "var(--accent)",
+        },
+    },
 	plugins: [],
 };
