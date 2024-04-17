769f1b82760358f50aea67f1902957843249111d
tokitouq <suneeth.since2005@gmail.com>
2024-04-17 22:57:53 +0530

    feat: fix chat context menu and add more options

--- frontend/src/components/shared/popups/chat-context-menu.tsx
+++ frontend/src/components/shared/popups/chat-context-menu.tsx
@@ -4,106 +4,118 @@ import { Accessor, Show, createSignal, onCleanup, onMount } from "solid-js";
 import { useShared } from "~/context/shared";
 import { FormatDate } from "~/functions/format-date";
 import Copy from "~/icons/copy";
+import Delete from "~/icons/delete";
 import Pencil from "~/icons/pencil";
 import Tick from "~/icons/tick";
 import { ChatMessage } from "~/types/chat.types";
 
 type Props = {
-	x: number;
-	y: number;
-	self: Accessor<boolean>;
-	message: Accessor<ChatMessage>;
-	onClose: (e: CustomEvent) => void;
+    x: number;
+    y: number;
+    self: Accessor<boolean>;
+    message: Accessor<ChatMessage>;
+    onClose: (e: CustomEvent) => void;
 };
 
 export const ChatContextMenu = (props: Props) => {
-	const { setEditMessage } = useShared();
-	const { x: PropX, y: PropY, message } = destructure(props);
+    const { setEditMessage } = useShared();
+    const { x: PropX, y: PropY, message } = destructure(props);
 
-	const [copied, setCopied] = createSignal(false);
-	const [x, setX] = createSignal(PropX());
-	const [y, setY] = createSignal(PropY());
+    const [copied, setCopied] = createSignal(false);
+    const [x, setX] = createSignal(PropX());
+    const [y, setY] = createSignal(PropY());
 
-	const dispatch = createEventDispatcher(props);
-	let ref: HTMLDivElement;
-	const formatedDate = new FormatDate(message()().timestamp)
-		.format_to_relative_time;
+    const dispatch = createEventDispatcher(props);
+    let ref: HTMLDivElement;
+    const formatedDate = new FormatDate(message()().timestamp)
+        .format_to_relative_time;
 
-	const handleOutsideClick = (event: MouseEvent) => {
-		if (!ref.contains(event.target as HTMLElement)) {
-			dispatch("close", {});
-		}
-	};
+    const handleOutsideClick = (event: MouseEvent) => {
+        if (!ref.contains(event.target as HTMLElement)) {
+            dispatch("close", {});
+        }
+    };
 
-	onMount(() => {
-		const rect = ref.getBoundingClientRect();
-		setX((prev) =>
-			prev > window.innerWidth - rect.width ? prev - rect.width : prev
-		);
-		setY((prev) =>
-			prev > window.innerHeight - rect.height ? prev - rect.height : prev
-		);
+    onMount(() => {
+        const rect = ref.getBoundingClientRect();
+        setX((prev) =>
+            prev > window.innerWidth - rect.width ? prev - rect.width : prev
+        );
+        setY((prev) =>
+            prev > window.innerHeight - rect.height ? prev - rect.height : prev
+        );
 
-		document.addEventListener("click", handleOutsideClick);
-		document.addEventListener("contextmenu", handleOutsideClick);
-	});
+        document.addEventListener("click", handleOutsideClick);
+        document.addEventListener("contextmenu", handleOutsideClick);
+    });
 
-	onCleanup(() => {
-		document.removeEventListener("click", handleOutsideClick);
-		document.removeEventListener("contextmenu", handleOutsideClick);
-	});
+    onCleanup(() => {
+        document.removeEventListener("click", handleOutsideClick);
+        document.removeEventListener("contextmenu", handleOutsideClick);
+    });
 
-	// Functions
-	const handleCopy = async () => {
-		await navigator.clipboard.writeText(props.message().content);
-		setCopied(true);
-	};
+    // Functions
+    const handleCopy = async () => {
+        await navigator.clipboard.writeText(props.message().content);
+        setCopied(true);
+    };
 
-	const handleEdit = () => {
-		setEditMessage(props.message());
-		dispatch("close", {});
-	};
+    const handleEdit = () => {
+        setEditMessage(props.message());
+        dispatch("close", {});
+    };
 
-	return (
-		<div
-			ref={ref!}
-			class="dropdown absolute"
-			style={{
-				top: `${y()}px`,
-				left: `${x()}px`
-			}}
-		>
-			<div class="btn-base-300 btn z-50 h-max min-h-max w-40 select-none flex-col gap-0 overflow-hidden rounded-xl border-none p-1 shadow">
-				<Show when={props.message().is_read && props.self()}>
-					<div class="flex w-full items-center gap-2 px-3 py-1.5 text-start text-accent">
-						<Tick
-							variant="double"
-							class="size-4"
-						/>
-						<span class="text-sm font-medium">
-							Seen {formatedDate}
-						</span>
-					</div>
-				</Show>
-				<Show when={props.self()}>
-					<div
-						onClick={handleEdit}
-						class="flex w-full items-center gap-2 rounded-lg px-3 py-1.5 text-start text-accent hover:bg-base-100"
-					>
-						<Pencil class="size-4" />
-						<span class="text-sm font-medium">Edit</span>
-					</div>
-				</Show>
-				<div
-					onClick={handleCopy}
-					class="flex w-full items-center gap-2 rounded-lg px-3 py-1.5 text-start text-accent hover:bg-base-100"
-				>
-					<Copy class="size-4" />
-					<span class="text-sm font-medium">
-						{copied() ? "Copied" : "Copy"}
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+    const handleDelete = () => {
+        console.log("Perform handle delete function");
+    };
+
+    return (
+        <div
+            ref={ref!}
+            class="dropdown absolute"
+            style={{
+                top: `${y()}px`,
+                left: `${x()}px`
+            }}
+        >
+            <div class="bg-base-200 z-50 h-max w-52 flex-col gap-0 overflow-hidden rounded-xl p-1">
+                <Show when={props.self()}>
+                    <button
+                        onClick={handleEdit}
+                        class="flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-100"
+                    >
+                        <Pencil class="size-4" />
+                        <span class="text-sm font-medium text-accent">Edit</span>
+                    </button>
+                </Show>
+                <button
+                    onClick={handleCopy}
+                    class="flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-100"
+                >
+                    <Copy class="size-4" />
+                    <span class="text-sm font-medium text-accent">
+                        {copied() ? "Copied" : "Copy Text"}
+                    </span>
+                </button>
+                <button
+                    onClick={handleDelete}
+                    class="flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-error hover:bg-base-100"
+                >
+                    <Delete class="size-4" />
+                    <span class="text-sm font-medium">Delete</span>
+                </button>
+                <Show when={props.message().is_read && props.self()}>
+                    <div class="border-t-2 border-neutral-300 mt-1 pt-2 flex w-full items-center gap-4 px-3 py-1.5 text-start text-neutral-100">
+                        <Tick
+                            variant="double"
+                            class="size-4"
+                        />
+                        <span class="text-sm font-medium">
+                            Seen {formatedDate}
+                        </span>
+                    </div>
+                </Show>
+            </div>
+        </div>
+    );
 };
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
+        boxShadow: {
+            DEFAULT: '0 4px 4px 0 rgb(0 0 0 / 0.04)',
+            l: '0 4px 4px 0 rgb(0 0 0 / 0.25)',
+            none: 'none',
+        },
         colors: {
             "primary": "rgb(var(--primary) / <alpha-value>)",
             "base-100": "rgb(var(--base-100) / <alpha-value>)",
@@ -11,6 +16,7 @@ module.exports = {
             "neutral-200": "rgb(var(--neutral-200) / <alpha-value>)",
             "neutral-300": "rgb(var(--neutral-300) / <alpha-value>)",
             "accent": "rgb(var(--accent) / <alpha-value>)",
+            "error": "rgb(var(--error) / <alpha-value>)",
         },
     },
 	plugins: [],
