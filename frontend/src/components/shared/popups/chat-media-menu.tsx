734818f042b23c4a3b6b73c2d692bb7bad4e14e9
tokitouq <suneeth.since2005@gmail.com>
2024-04-20 01:01:16 +0530

    fix: chat media menu

--- frontend/src/components/shared/popups/chat-media-menu.tsx
+++ frontend/src/components/shared/popups/chat-media-menu.tsx
@@ -5,7 +5,7 @@ export default function ChatMediaMenu() {
 	return <>
         <label
             for="image-file-input"
-            class="flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-200/75"
+            class="cursor-pointer flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-200/75"
         >
             <Photo class="size-4" />
             <span class="text-sm font-medium text-accent">
@@ -14,7 +14,7 @@ export default function ChatMediaMenu() {
         </label>
         <label
             for="gif-file-input"
-            class="flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-200/75"
+            class="cursor-pointer flex w-full items-center gap-4 rounded-lg px-3 py-1.5 text-start text-neutral-100 hover:bg-base-200/75"
         >
             <Gif class="size-4" />
             <span class="text-sm font-medium text-accent">
