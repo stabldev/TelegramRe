cbd11aa304804a80435758b71f77a3f21288a310
tokitouq <suneeth.since2005@gmail.com>
2024-05-10 20:22:57 +0530

    feat: complete custom tailwind checkbox

--- frontend/src/components/pages/auth/email-form.tsx
+++ frontend/src/components/pages/auth/email-form.tsx
@@ -94,13 +94,25 @@ const EmailForm = (props: Props) => {
 						Email address
 					</span>
 				</label>
-				<label class="flex items-center md:gap-3">
+				<label class="relative flex items-center md:gap-3">
 					<input
 						name="keep-me"
 						type="checkbox"
 						checked
 						class="relative peer appearance-none md:size-4 border-2 border-neutral-300 cursor-pointer rounded bg-transparent checked:bg-primary checked:border-0"
 					/>
+					<svg
+					    class="absolute md:size-3 text-accent self-center start-0.5 hidden peer-checked:block"
+					    xmlns="http://www.w3.org/2000/svg"
+					    viewBox="0 0 24 24"
+					    fill="none"
+					    stroke="currentColor"
+					    stroke-width="4"
+					    stroke-linecap="round"
+					    stroke-linejoin="round"
+					 >
+					    <polyline points="20 6 9 17 4 12"></polyline>
+					</svg>
 					<span class="text-neutral-100">Keep me signed in</span>
 				</label>
 				<button class="bg-primary text-accent uppercase w-full h-12 rounded-xl">Next</button>
