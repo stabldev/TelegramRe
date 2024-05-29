a29273c2bb4526c54817c523a23690cf1bdae702
tokitouq <suneeth.since2005@gmail.com>
2024-05-29 15:41:02 +0530

    refactor: add param match filter for routes

--- frontend/src/routes/(user)/[user].tsx
+++ frontend/src/routes/(user)/[user].tsx
@@ -1,5 +1,5 @@
 import { Title } from "@solidjs/meta";
-import { useParams } from "@solidjs/router";
+import { RouteDefinition, RouteSectionProps } from "@solidjs/router";
 import { Show } from "solid-js";
 import ChatScreen from "~/components/pages/chat";
 import ChatSidebar from "~/components/shared/chat/chat-sidebar";
@@ -7,12 +7,17 @@ import { useChat } from "~/context/chat";
 import { useShared } from "~/context/shared";
 import DefaultLayout from "~/layouts/default-layout";
 
-const UserChat = () => {
+export const route = {
+  	matchFilters: {
+  		user: (v: string) => v.length > 1 && v.includes("@")
+  	}
+} satisfies RouteDefinition;
+
+const UserChat = (props: RouteSectionProps) => {
 	const { showSidebar } = useShared();
 	const { activeRoom } = useChat();
 
-	const params = useParams<{ user: string }>();
-	const title = activeRoom()?.member[0].full_name ?? params.user;
+	const title = activeRoom()?.member[0].full_name ?? props.params.user;
 
 	return (
 		<>
