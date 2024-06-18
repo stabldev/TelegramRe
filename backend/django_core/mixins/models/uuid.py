98b597e132e64044d3cd2c51cadf98eade4fd0ea
tokitouq <suneeth.since2005@gmail.com>
2024-06-18 14:38:44 +0530

    fix: UUIDMixin default id generation range

--- backend/django_core/mixins/models/uuid.py
+++ backend/django_core/mixins/models/uuid.py
@@ -5,7 +5,7 @@ from django_core.utilities.numeric_uuid import generate_numeric_uuid
 
 class UUIDMixin(models.Model):
     id = models.CharField(
-        primary_key=True, default=generate_numeric_uuid, editable=False
+        primary_key=True, default=generate_numeric_uuid(), editable=False
     )
 
     class Meta:
--- backend/django_core/utilities/numeric_uuid.py
+++ backend/django_core/utilities/numeric_uuid.py
@@ -4,4 +4,4 @@ import random
 def generate_numeric_uuid(length=10):
     if length <= 0:
         raise ValueError("length must be positive integer")
-    return str(random.randint(10**length, (10 ** (length - 1)) - 1))
+    return str(random.randint(10 ** (length - 1), (10**length) - 1))
