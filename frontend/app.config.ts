af48210f552e7365cf528b8118a2022a87d09316
tokitouq <suneeth.since2005@gmail.com>
2024-06-23 10:31:58 +0530

    fix: frontend service hmr issue

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -3,5 +3,17 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     preset: "vercel"
+  },
+  vite({ router }) {
+    if (router === "client") {
+      return {
+        server: {
+          hmr: {
+            port: 4000
+          }
+        }
+      };
+    }
+    return {};
   }
 });
