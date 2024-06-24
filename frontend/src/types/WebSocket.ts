ebed58d5be3979cf6ea1dfb67283ef5724012e93
tokitouq <suneeth.since2005@gmail.com>
2024-06-24 14:11:42 +0530

    fix: data type for actions

--- frontend/src/types/WebSocket.ts
+++ frontend/src/types/WebSocket.ts
@@ -1,9 +1,16 @@
 import type { ChatMessage } from "./chat";
 import type { OnlineUser } from "./user";
 
+type DataActions =
+  | "online_users"
+  | "message"
+  | "edit_message"
+  | "read_room"
+  | "read_message";
+
 export type WebSocketData = {
   // TODO: add seperate type for "action"
-  action: "online_message";
+  action: DataActions;
 } & Partial<{
   message: ChatMessage;
   online_users_list: OnlineUser[];
