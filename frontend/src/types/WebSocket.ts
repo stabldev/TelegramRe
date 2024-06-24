d2142fb456d69f2a93ee0b0cfcb2f122914a3529
tokitouq <suneeth.since2005@gmail.com>
2024-06-24 13:57:37 +0530

    refactor: websocket type

--- frontend/src/types/WebSocket.ts
+++ frontend/src/types/WebSocket.ts
@@ -4,6 +4,7 @@ import type { OnlineUser } from "./user";
 export type WebSocketData = {
   // TODO: add seperate type for "action"
   action: "online_message";
-  message?: ChatMessage;
-  online_users_list?: OnlineUser[];
-};
+} & Partial<{
+  message: ChatMessage;
+  online_users_list: OnlineUser[];
+}>;
